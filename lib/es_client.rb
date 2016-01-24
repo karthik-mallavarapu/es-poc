79530370f905b1975ef62266c34af1e9679a4a81
karthik <karthik@bigparser-jenkins.com>
2016-01-24 15:20:27 +0000

    Adds retry to http requests

--- lib/es_client.rb
+++ lib/es_client.rb
@@ -25,14 +25,22 @@ class EsClient
   def create_index
     res = HttpClient.head(index_url)
     HttpClient.delete(index_url) if res.code == 200
-    res = HttpClient.post(index_url, body: index_settings.to_json)
-    raise "Could not create index" if res.code != 200
+    begin
+      res = HttpClient.post(index_url, body: index_settings.to_json)
+      raise "Could not create index" if res.code != 200
+    rescue => e
+      res = HttpClient.post(index_url, body: index_settings.to_json)
+    end
   end
 
   def create_mapping
     mapping = get_mapping_properties
-    res = HttpClient.put("#{index_url}/_mapping/#{doc_type}", body: mapping.to_json)
-    raise "Could not create mapping #{res.body}" unless res.code == 200
+    begin
+      res = HttpClient.put("#{index_url}/_mapping/#{doc_type}", body: mapping.to_json)
+      raise "Could not create mapping #{res.body}" unless res.code == 200
+    rescue => e
+      res = HttpClient.put("#{index_url}/_mapping/#{doc_type}", body: mapping.to_json)
+    end
   end
 
   def populate_data(parser, grid_id)
@@ -47,8 +55,12 @@ class EsClient
         end
         data_rows_json += index_metadata + "\n" + JSON.generate(data_row) + "\n"
       end
-      res = HttpClient.post("#{index_url}/#{doc_type}/_bulk?routing=#{grid_id}", body: data_rows_json)
-      raise "Data index failure #{res.to_s}" unless (res.code == 201 || res.code == 200)
+      begin
+        res = HttpClient.post("#{index_url}/#{doc_type}/_bulk?routing=#{grid_id}", body: data_rows_json)
+        raise "Data index failure #{res.to_s}" unless (res.code == 201 || res.code == 200)
+      rescue => e
+        res = HttpClient.post("#{index_url}/#{doc_type}/_bulk?routing=#{grid_id}", body: data_rows_json)
+      end
     end
   end
 
